d2340720053302d126aa5008fc71e2a79c7349f2
SIMS IOT Devices <92898043+SIMS-IOT-Devices@users.noreply.github.com>
2025-03-23 22:28:56 +0200

    Add files via upload

---
+++ spi_rpi_3.py
@@ -0,0 +1,15 @@
+import spidev
+import time
+
+# Create an SPI instance
+spi = spidev.SpiDev()
+spi.open(0, 0)  # Bus 0, Device 0 (CE0)
+spi.max_speed_hz = 500000  # Set speed (adjust as needed)
+spi.mode = 0  # SPI mode (0-3)
+
+# Send triger
+response = spi.xfer2([0x01])
+print("Received:", hex(response[0]))
+
+# Close SPI
+spi.close()
